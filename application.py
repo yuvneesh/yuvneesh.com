7252fd713b09d0306ba4bc2e45ba3d470ed7c064
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2024-02-22 02:23:40 -0500

    initial commit

---
+++ application.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """render the homepage"""
+    return "<html><body><h1>Welcome to my app!</h1></body></html>\n"
