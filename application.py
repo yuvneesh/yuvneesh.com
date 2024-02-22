526a8dbfa3480d6cba8c161a8fe7c29d79d3b6ae
Yuvneesh Kashyap <yuvneeshk53@gmail.com>
2024-02-22 02:28:39 -0500

    templates connected to app

--- application.py
+++ application.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """render the homepage"""
-    return "<html><body><h1>Welcome to my app!</h1></body></html>\n"
+    # return "<html><body><h1>Welcome to my app!</h1></body></html>\n"
+    return render_template("home.html")
